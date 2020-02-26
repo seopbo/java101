5d53a8301cad7141c2bc8f6d63d0997f15d1d05c
aisolab <bsk0130@gmail.com>
2020-02-26 15:34:42 +0900

    Chapter05 객체 만들기

--- Chapter05/src/BankAccount.java
+++ Chapter05/src/BankAccount.java
@@ -5,19 +5,36 @@ public class BankAccount {
     // 파라미터: 입금할 액수
     // 리턴: 성공 여부 (불린)
     boolean deposit (int amount) {
+        if (amount < 0 || owner.cashAmount < amount) {
+            System.out.printf("입금 실패입니다. 잔고: %d원, 현금: %d원\n", balance, owner.cashAmount);
+            return false;
+        } else {
+            balance = balance + amount;
+            owner.cashAmount = owner.cashAmount - amount;
+            System.out.printf("%d원 입금하였습니다. 잔고: %d원, 현금: %d원\n", amount, balance, owner.cashAmount);
+            return true;
+        }
 
     }
 
     // 파라미터: 출금할 액수
     // 리턴: 성공 여부 (불린)
     boolean withdraw (int amount) {
-
+        if (amount < 0 || amount > balance) {
+            System.out.printf("출금 실패입니다. 잔고: %d원, 현금: %d원\n", balance, owner.cashAmount);
+            return false;
+        } else {
+            balance = balance - amount;
+            owner.cashAmount = owner.cashAmount + amount;
+            System.out.printf("%d원 출금하였습니다. 잔고: %d원, 현금: %d원\n", amount, balance, owner.cashAmount);
+            return true;
+        }
     }
 
-    // 첫 번째 파라미터: 받는 사람 (Person)
-    // 두 번째 파라미터: 이체할 금액 (정수)
-    // 리턴: 성공 여부 (불린)
-    boolean transfer(Person to, int amount) {
-
-    }
+    // 첫 번째 파라미터 : 받는 사람(Person)
+    // 두 번째 파라미터 : 이체할 금액(정수)
+    // 리턴 : 성공여부(불린)
+    // boolean transfer(Person to, int amount) {
+    // (다음 과제에서 사용하므로, 우선 skip합니다.)
+    // }
 }
--- Chapter05/src/BankDriver.java
+++ Chapter05/src/BankDriver.java
@@ -2,5 +2,29 @@ public class BankDriver {
     public static void main(String[] args) {
         // 사람 선언
         Person p1 = new Person();
+        p1.name = "김신의";
+        p1.age = 28;
+        p1.cashAmount = 30000;
+
+        Person p2 = new Person();
+        p2.name = "김보섭";
+        p2.age = 32;
+        p2.cashAmount = 100000;
+
+        // 은행 계좌 생성
+        BankAccount a1 = new BankAccount();
+        a1.balance = 100000;
+        p1.account = a1;
+        a1.owner = p1;
+
+        BankAccount a2 = new BankAccount();
+        a2.balance = 500000;
+        p2.account = a2;
+        a2.owner = p2;
+
+        p2.account.deposit(30000);
+        p2.account.withdraw(170000);
+        p2.account.deposit(620000);
+        p2.account.withdraw(890000);
     }
 }
